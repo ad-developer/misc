db8cf7d1ff4a8a79f2118e0caf3746588d3fca7a
A.D <A.D.@PHYAdmins-MacBook-Air.local>
2017-06-25 19:19:31 -0700

    next

--- dnd/js/test.js
+++ dnd/js/test.js
@@ -14,11 +14,50 @@
 
   https://developer.mozilla.org/en-US/docs/Web/Events/dragover
 */
+/*
+  Transformations:
+
+  <!--
+    Placeholder checkbox (dynamic)
+    Transformation: ad-plc-checkbox - hook attribute
+    To dest: remove hide class
+    To src: add hide class
+  -->
+  <!--
+    Placeholder icon fixed width (dynamic)
+    Transformation: ad-plc-required, ad-plc-required-cont - hook
+      attributes
+    To dest: remove hide class, keep hide on content
+    To src: add hide class, add hide class to content
+  -->
+  <!--
+    Placeholder chips (dynamic)
+    Transformation: ad-plc-chip, ad-plc-chip-cont - hook attributes
+    To dest: do nothing
+    To src: add hide class, remove all content
+  -->
+  <!--
+    Type icon (dynamic)
+    Transformation: ad-dnd-comp-type - hook attribute
+    To dest: remove ad-right-24 class
+    To src: add ad-right-24 class
+  -->
+*/
 
 const htmStrings = {
-  CHECK_BOX: '<span class=ad-right-24><i ad-checkbox class="list-icon checked"role=button><svg viewBox="0 0 24 24"><path d="M0 0h24v24H0z"fill=none /><path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg> </i><i ad-checkbox class="list-icon hide"role=button ad-hidden><svg viewBox="0 0 24 24"><path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/><path d="M0 0h24v24H0z"fill=none /></svg></i></span>'
-
+  CHECK_BOX: '<span class=ad-right-24><i ad-checkbox class="list-icon checked"role=button><svg viewBox="0 0 24 24"><path d="M0 0h24v24H0z"fill=none /><path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg> </i><i ad-checkbox class="list-icon hide"role=button ad-hidden><svg viewBox="0 0 24 24"><path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/><path d="M0 0h24v24H0z"fill=none /></svg></i></span>',
+  CHIP: '<div class="ad-chip ad-chip-red" ad-plc-chip-cont></div>'
 };
+
+const dndTestStrings = {
+  PLC_CHECKBOX: 'ad-plc-checkbox',
+  PLC_REQUIRED: 'ad-plc-requaired',
+  DND_COMP_TYPE: 'ad-dnd-comp-type'
+};
+const dndTestCssClasses = {
+  HIDE: 'hide',
+  RIGHT_24: 'ad-right-24'
+}
 
 // Drop Item handler....
 let dropItemHnd = function(e, callback) {
@@ -30,9 +69,16 @@ let dropItemHnd = function(e, callback) {
   let dropInst = ADDnd.getInstance(el);
   dropInst.deactivate();
   dropInst.activate('all');
+
   // transformation
-  let grip = dropInst.root.querySelector('[ad-dnd-grip]');
-  util.insertAfter(htmStrings.CHECK_BOX, grip);
+  let checkbox = dropInst.root.querySelector(`[${dndTestStrings.PLC_CHECKBOX}]`);
+  checkbox.classList.remove(dndTestCssClasses.HIDE);
+
+  let req = dropInst.root.querySelector(`[${dndTestStrings.PLC_REQUIRED}]`);
+  req.classList.remove(dndTestCssClasses.HIDE);
+
+  let comT = dropInst.root.querySelector(`[${dndTestStrings.DND_COMP_TYPE}]`);
+  comT.classList.remove(dndTestCssClasses.RIGHT_24);
 
   parent.insertBefore(el, root);
   callback(true);
@@ -56,9 +102,19 @@ ADDnd.attachToMany('[ad-dnd-src]', {
     let id = e.dataTransfer.getData("text");
     let el = document.getElementById(id);
     let dropInst = ADDnd.getInstance(el);
+    let root = this.root;
     dropInst.deactivate();
     dropInst.activate('drag');
+
     // transformation
+    let checkbox = dropInst.root.querySelector(`[${dndTestStrings.PLC_CHECKBOX}]`);
+    checkbox.classList.add(dndTestCssClasses.HIDE);
+
+    let req = dropInst.root.querySelector(`[${dndTestStrings.PLC_REQUIRED}]`);
+    req.classList.add(dndTestCssClasses.HIDE);
+
+    let comT = dropInst.root.querySelector(`[${dndTestStrings.DND_COMP_TYPE}]`);
+    comT.classList.add(dndTestCssClasses.RIGHT_24);
 
     callback(false);
   }
@@ -71,14 +127,27 @@ ADDnd.attachToMany('[ad-dnd-dest]', {
     let id = e.dataTransfer.getData("text");
     let el = document.getElementById(id);
     let dropInst = ADDnd.getInstance(el);
+    let root = this.root;
     dropInst.deactivate();
     dropInst.activate('all');
     // transformation
+
+    let checkbox = dropInst.root.querySelector(`[${dndTestStrings.PLC_CHECKBOX}]`);
+    checkbox.classList.remove(dndTestCssClasses.HIDE);
+
+    let req = dropInst.root.querySelector(`[${dndTestStrings.PLC_REQUIRED}]`);
+    req.classList.remove(dndTestCssClasses.HIDE);
+
+    let comT = dropInst.root.querySelector(`[${dndTestStrings.DND_COMP_TYPE}]`);
+    comT.classList.remove(dndTestCssClasses.RIGHT_24);
 
     callback(false);
   }
 });
 
+// Register checkbox
+
+ADCheckbox.attachToMany('[ad-checkbox]');
 
 document.getElementById('btn').addEventListener('click', function(){});
 
