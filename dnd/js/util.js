fb10cfa5daed4cc5a34078e48d1ab0ee0697238c
ad-developer <BusinessSubscription@gmail.com>
2017-06-30 16:59:20 -0700

    Update util.js

--- dnd/js/util.js
+++ dnd/js/util.js
@@ -4,32 +4,13 @@ const util = {
   el: function(id) {
     return document.getElementById(id);
   },
-  closest: function(domEl, attr)  {
-    if(domEl.hasAttribute(attr)){
-      return domEl;
-    }
-    let el = domEl.parentElement;
-    let res = null;
-    while (el.parentElement != null) {
-      if(el.hasAttribute(attr)){
-        res = el;
-        break;
+  closest: function(el, sel)  {
+    while(el) {
+      if (el.matches(sel)) {
+          return el;
       }
       el = el.parentElement;
     }
-    return res;
-  },
-  getParentOfType: function(el, type){
-    if(el.nodeName === type){
-      return el;
-    }
-    let nodeName = el.parentElement.nodeName;
-    let parentElement = el.parentElement;
-    while (nodeName !== type) {
-      parentElement = parentElement.parentElement;
-      nodeName = parentElement.nodeName;
-    }
-    return parentElement;
   },
   insertAfter: function(newNode, referenceNode) {
     let el = document.createElement('div');
