a2ab22c0167561a1f31cd6efa4da69c9abc0509a
A.D <A.D.@PHYAdmins-MacBook-Air.local>
2017-07-02 21:54:17 -0700

    next

--- uploader/js/test.js
+++ uploader/js/test.js
@@ -1,1 +1,7 @@
 ADUploader.attachTo(document.getElementById('uploader'),{add: true});
+
+let btn = document.getElementById('btnData');
+btn.addEventListener('click', (e)=>{
+  let inst = ADUploader.getInstance(document.getElementById('uploader'));
+  alert(inst.getFormData());
+});
--- uploader/js/uploader.js
+++ uploader/js/uploader.js
@@ -1,8 +1,8 @@
 /* ========================================================================
  * uploader.js
- * Simple file upload coltrol
+ * Simple file uploader coltrol.
  * ========================================================================
- * Copyright 2017 A. D.
+ * Copyright 2017 A. D. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,15 +34,37 @@
     change
 */
 
-const cssClasses = {
+const util = {
+  closest(el, sel)  {
+    while(el) {
+      if (el.matches(sel)) {
+          return el;
+      }
+      el = el.parentElement;
+    }
+  }
+};
 
+const cssClasses = {
+  BLNK: 'ad-uploader-blink',
+  PRG_RUN: 'ad-progress-run'
 };
 
 const attributes = {
-  ATR_ADD: 'ad-uploader-add',
-  ATR_UPLOAD: 'ad-uploader-upload',
-  ATR_DEL: 'ad-uploader-del',
-  ATR_UPL_CTR: 'ad-uploader-control'
+  ADD: 'ad-uploader-add',
+  UPLOAD: 'ad-uploader-upload',
+  DEL: 'ad-uploader-del',
+  CTR: 'ad-uploader-control',
+  LIST: 'ad-uploader-list',
+  ITEM: 'ad-uploader-item',
+  ITEM_LINK: 'ad-item-link',
+  ITEM_DEL: 'ad-item-delete',
+  ITEM_DEL_CONF: 'ad-item-conf',
+  TTL: 'title'
+};
+
+const strings = {
+  CONF: 'Confirm file deletion.'
 };
 
 class ADUploader {
@@ -50,7 +72,7 @@ class ADUploader {
     let instance = new ADUploader(root, opt);
     // attach instance to the root
     root.ad = root.ad || {};
-    root.ad['addnd'] = instance;
+    root.ad['aduploader'] = instance;
     return instance;
   }
   static getInstance(root) {
@@ -60,19 +82,24 @@ class ADUploader {
     this.root_ = root;
     this.opt_ = opt;
 
+    this.formData_ = new FormData();
+    this.fileList_ = [];
+
     this.listenerInfos_ = [
-      {re:'uploadClick', ev:'click', ctr: attributes.ATR_UPLOAD},
-      {re:'change', ev:'change', ctr: 'root'},
-      {re:'deleteFile', ev:'click', ctr: attributes.ATR_DEL},
+      {re:'uploadClick', ev:'click', ctr: attributes.UPLOAD },
+      {re:'change', ev:'change', ctr: 'root' },
+      {re:'deleteFile', ev:'click', ctr: attributes.DEL },
+      {re: 'controlChange', ev:'change', ctr: attributes.CTR }
     ];
     this.listeners_ = {
       uploadClick: (e) => this.uploadBtnClick_(e),
       change: (e) => this.uploaderChange_(e),
-      deleteFile:(e) => this.deleteFile_(e),
+      deleteFile:(e) => this.deleteFiles_(e),
+      controlChange:(e) => this.controlChange_(e)
     };
     if(opt.add){
       this.listeners_.addClick = (e) => this.addClick_(e);
-      this.listenerInfos_.push({re:'addClick', ev:'click', ctr: attributes.ATR_ADD});
+      this.listenerInfos_.push({re:'addClick', ev:'click', ctr: attributes.ADD});
     }
     this.initialize();
   }
@@ -100,14 +127,12 @@ class ADUploader {
     el.addEventListener(entType, handler);
   }
   getFormData(){
-
+    return this.formData_;
   }
   getFileList(){
-
+    return this.fileList_;
   }
-  setFileList(){
-
-  }
+  setFileList(){}
   emit(evtType, evtData, shouldBubble = false) {
    let evt;
    if (typeof CustomEvent === 'function') {
@@ -123,17 +148,84 @@ class ADUploader {
    this.root_.dispatchEvent(evt);
   }
 
+  setConfirm_(domBtn) {
+    let atr = domBtn.getAttribute(attributes.ITEM_DEL_CONF);
+    let conf = false;
+    if(atr) {
+      domBtn.classList.remove(cssClasses.BLNK);
+      domBtn.classList.add(cssClasses.PRG_RUN);
+      conf = true;
+    } else {
+      domBtn.setAttribute(attributes.ITEM_DEL_CONF, true);
+      domBtn.classList.add(cssClasses.BLNK);
+      domBtn.setAttribute(attributes.TTL, strings.CONF);
+    }
+    return conf;
+  }
+
+  getRecTmp_(){
+    // Wire delete event for the record here.
+    let tmp = `  <li class="ad-uploader-item" ad-uploader-item>
+        <a href="#" class="ad-uploader-link" ad-item-link></a>
+        <div class="ad-uploader-mask"></div>
+        <a href="#" class="ad-uploader-delete" ad-item-delete role="button">
+          <div class="ad-progress">
+            <div class="ad-progress-runner"></div>
+          </div>
+          <i class="ad-icon">
+            <svg viewBox="0 0 24 24">
+                <path d="M0 0h24v24H0V0z" fill="none"/>
+                <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/>
+                <path d="M0 0h24v24H0z" fill="none"/>
+            </svg>
+          </i>
+        </a>
+      </li>`;
+    let con = document.createElement('div');
+    con.innerHTML = tmp;
+    let btn = con.querySelector(`[${attributes.ITEM_DEL}]`);
+    btn.addEventListener('click', (e) => this.deleteFile_(e));
+    return con.firstElementChild;
+  }
+  addFile_(file) {
+    let rec = this.getRecTmp_();
+    let item = rec.querySelector(`[${attributes.ITEM_LINK}]`);
+    item.textContent = file.name;
+    let listCon = this.root_.querySelector(`[${attributes.LIST}]`);
+    listCon.appendChild(rec);
+  }
+  addFormData_(file) {
+    this.formData_.append('files', file, file.name);
+  }
+  addFiles_(){
+    let upl = this.root_.querySelector(`[${attributes.CTR}]`);
+    let files = upl.files;
+    [].forEach.call(files, (f) => {
+      this.addFile_(f);
+      this.addFormData_(f);
+    });
+    // Clear control to accept more files
+    // if needed...
+    upl.value = '';
+  }
+  removeFile_(domBtn) {
+    let rec = util.closest(domBtn, `[${attributes.ITEM}]`);
+    rec.remove();
+  }
   // Event handlers
-  uploadBtnClick_(e) {
-
-  }
-  uploaderChange_(e) {
-
-  }
+  uploadBtnClick_(e) {}
+  uploaderChange_(e) {}
+  deleteFiles_(e) {}
   deleteFile_(e) {
-
+    let btn = e.currentTarget;
+    if(this.setConfirm_(btn)){
+      this.removeFile_(btn);
+    }
   }
   addClick_(e) {
-    document.querySelector(`[${attributes.ATR_UPL_CTR}]`).click();
+    document.querySelector(`[${attributes.CTR}]`).click();
+  }
+  controlChange_(e) {
+    this.addFiles_();
   }
 }
